67fd0105a860833e46435864fda2f29170b0f2e4
Franco_Ojeda <24martin94@gmail.com>
2024-02-19 18:30:10 -0300

    Habilitacion de rutas 😅

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use('/api', router)
+app.use('/api', router)
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Endpoint no encontrado' })
