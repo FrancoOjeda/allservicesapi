49545c63965f8880015af063ddd0a9db6f68340f
Franco_Ojeda <24martin94@gmail.com>
2024-03-04 22:59:01 -0300

    modificacion de la home

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express');
 const express = require('express')
 const cors = require('cors')
 const hbs = require('express-handlebars')
 const path = require('path')
 require('dotenv').config()
-const swaggerUi = require('swagger-ui-express');
 // const { autorizarUsuario } = require('./src/autorizacion/autorizarUsuario.js')
 
 // const { createClient } = require("@libsql/client")
